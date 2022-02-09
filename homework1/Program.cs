1c26c9b1de9cc7573472f36b58be6a951c5fa054
Ismayil9918 <ismayilbi@code.edu.az>
2022-02-09 07:57:07 +0400

    added Signed-off-by: Ismayil9918 <ismayilbi@code.edu.az>

---
+++ homework1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+string txt = "Hello waorld";
+for (int i = 0; i < txt.Length; i++)
+    if (txt[i] =='a')
+{
+        Console.WriteLine("a var");
+        
+            
+}
