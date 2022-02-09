33afbfb79a350c29e163f6d6395de4e48e889608
Ismayil9918 <ismayilbi@code.edu.az>
2022-02-09 08:22:36 +0400

    added Signed-off-by: Ismayil9918 <ismayilbi@code.edu.az>

---
+++ homework2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num = 4;
+if (num % 2 == 0)
+{
+    Console.WriteLine("eded cütdür");
+
+
+}
+else
+{
+    Console.WriteLine( "eded tekdir");
+}
